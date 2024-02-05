523cc01d478d1682bb4a6bd16958bbcaac6a2bf9
Jacob-Vance <54328477+Jacob-Vance@users.noreply.github.com>
2024-02-05 17:46:11 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -8,13 +8,13 @@ $(document).ready(function(){
 
       let dogeLeft = $("img").position().left;
 
-      if (dogeLeft > 100, dogeLeft <= 1000) {
+      if (dogeLeft > 100, dogeLeft <= 1500) {
         console.log(dogeLeft);
 
         $("p").text("Wow, you got " + dogeLeft + " points. Good for you.");
 
       }
-      if (dogeLeft > 1000) {
+      if (dogeLeft > 1500) {
         console.log(dogeLeft);
 
         $("p").text("Wow, you got " + dogeLeft + " points. You win.");
