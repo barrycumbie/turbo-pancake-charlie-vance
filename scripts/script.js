09d91472f5bc5404a01933dbf67e1a1a76c26e01
Jacob-Vance <54328477+Jacob-Vance@users.noreply.github.com>
2024-02-06 12:16:20 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,25 +2,30 @@ $(document).ready(function(){
   
   console.log("Top: " + $("img").position().top + "Left: " + $("img").position().left);
   
-  $("img").draggable({
-    stop: function () {
-      console.log("stopping???");
+  // $("img").draggable({
+  //   stop: function () {
+  //     console.log("stopping???");
 
-      let dogeLeft = $("img").position().left;
+  //     let dogeLeft = $("img").position().left;
 
-      if (dogeLeft > 100, dogeLeft <= 1500) {
-        console.log(dogeLeft);
+  //     if (dogeLeft > 100, dogeLeft <= 1000) {
+  //       console.log(dogeLeft);
 
-        $("p").text("Wow, you got " + dogeLeft + " points. Good for you.");
+  //       $("p").text("Wow, you got " + dogeLeft + " points. Good for you.");
 
-      }
-      if (dogeLeft > 1500) {
-        console.log(dogeLeft);
+  //     }
+  //     if (dogeLeft > 1000) {
+  //       console.log(dogeLeft);
 
-        $("p").text("Wow, you got " + dogeLeft + " points. You Win.");
+  //       $("p").text("Wow, you got " + dogeLeft + " points. You win.");
 
-      }
-    },
-  });
+  //     }
+  //   },
+  // });
 
+ 
+  $( "#draggable3" ).draggable(
+    { 
+      axis: "x" 
+    });
 });
